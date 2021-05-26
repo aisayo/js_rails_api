ea171fb635803de638b07832b417709325c43ce2
Aysan Isayo <aisayo@icloud.com>
2021-05-26 14:51:12 -0700

    cors

---
+++ server_side/db/migrate/20210526205312_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :publisher
+      t.datetime :publish_date
+      t.integer :page_count
+      t.belongs_to :author, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
